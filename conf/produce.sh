777eaed2c34346edf61e67a03ccf3d442793257c
chenwx <chenwx716@163.com>
2019-04-19 13:53:27 +0800

    gitignore produce file

--- conf/produce.sh
+++ conf/produce.sh
@@ -2,7 +2,7 @@
 
 work_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
 cd $work_dir
-
+mkdir $work_dir/src/conf
 cp $work_dir/conf/produce.yaml $work_dir/src/conf/conf.yaml
 cp $work_dir/conf/produce.key $work_dir/src/conf/conf.key
 cp $work_dir/conf/produce.py $work_dir/src/config.py
@@ -19,7 +19,5 @@ rm $work_dir/src/log/work.log
 echo '-------------------------build start--------------------'
 docker build -t chenwx/$container:$version -f conf/bmgr.dockerfile .
 
-# docker run --name $container -h $container --net="host" -d chenwx/$container:$version
-# docker run --name $container -h $container --net="host" -d chenwx/$container:1.0.2
-
+docker run --name $container -h $container --net="host" -d chenwx/$container:$version
 # docker run --name bmgr -h bmgr --net="host" -d chenwx/bmgr:0.2
